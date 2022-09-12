71e9a8be12619ba16eda44fe68db758b88c3a4ed
Victor Darkwah-Owusu <vodarkwah58@gmail.com>
2022-09-12 05:03:41 -0700

    trying to fix error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except IndexError:
             break
         except (ValueError, TypeError):
-            pass
+            continue
 
     print('')
     return tot
